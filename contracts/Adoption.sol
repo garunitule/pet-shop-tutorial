0088e4c9f06032130f3290f5a9fdbf1dc719c5bf
garunitule <youtaiguhe@gmail.com>
2022-08-01 11:03:54 +0900

    マジックナンバー削除

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,11 +1,14 @@
 pragma solidity >=0.4.22 <0.8.16;
 
+// 犬と飼い主の紐付け、読み込み
 contract Adoption {
+    uint constant petIdMin = 0;
+    uint constant petIdMax = 15;
     address[16] public adopters;
 
     function adopt(uint petId) public returns (uint) {
         // adoptersのキーに収まるか判定
-        require(petId >= 0 && petId <= 15);
+        require(petId >= petIdMin && petId <= petIdMax);
 
         adopters[petId] = msg.sender;
 
